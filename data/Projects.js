05fd8a1e8b603cd5c702551978d2a3c6e8f2c8c3
Kumar Ankush <000.kumarankush@gmail.com>
2023-02-25 00:16:50 +0530

    initial commit

---
+++ data/Projects.js
@@ -0,0 +1,47 @@
+export const Projects = [
+  {
+    id: 1,
+    name: "Space Game",
+    img: "sgcid",
+    demo: "https://sgcid.makcnadlkcnalk.ss",
+    sourceCode: "https://github.com/gdsc/sgcid",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla alias accusantium commodi quia rerum reprehenderit similique, ipsa cupiditate vel voluptates sapiente, unde ducimus dignissimos saepe consequuntur possimus maxime voluptatibus laudantium.",
+  },
+  {
+    id: 2,
+    name: "Weather App",
+    img: "weather-app",
+    demo: "https://weather.jsnjsnd.sss",
+    sourceCode: "https://github.com/gdsc/weather-app",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla alias accusantium commodi quia rerum reprehenderit similique, ipsa cupiditate vel voluptates sapiente, unde ducimus dignissimos saepe consequuntur possimus maxime voluptatibus laudantium.",
+  },
+  {
+    id: 3,
+    name: "Coming Soon Project",
+    img: "coming-soon",
+    demo: "https://",
+    sourceCode: "https://",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla alias accusantium commodi quia rerum reprehenderit similique, ipsa cupiditate vel voluptates sapiente, unde ducimus dignissimos saepe consequuntur possimus maxime voluptatibus laudantium.",
+  },
+  {
+    id: 4,
+    name: "Coming Soon Project",
+    img: "coming-soon",
+    demo: "https://",
+    sourceCode: "https://",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla alias accusantium commodi quia rerum reprehenderit similique, ipsa cupiditate vel voluptates sapiente, unde ducimus dignissimos saepe consequuntur possimus maxime voluptatibus laudantium.",
+  },
+  {
+    id: 5,
+    name: "Coming Soon Project",
+    img: "coming-soon",
+    demo: "https://",
+    sourceCode: "https://",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla alias accusantium commodi quia rerum reprehenderit similique, ipsa cupiditate vel voluptates sapiente, unde ducimus dignissimos saepe consequuntur possimus maxime voluptatibus laudantium.",
+  },
+];
